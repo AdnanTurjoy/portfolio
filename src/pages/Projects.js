698b21a2cd4a03e22cc1e0c15f7b059266a2f559
AdnanTurjoy <aturjoy4@gmail.com>
2022-11-16 11:10:45 +0600

    loaded add

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,14 +5,17 @@ import ProjectItem from "../components/ProjectItem";
 //import { ProjectList } from "../Data/ProjectList";
 
 import "../styles/Projects.css";
-
+import { MoonLoader } from "react-spinners";
 function Projects() {
+  const [isLoading, setIsLoading] = useState(false);
   const [projectList, setProjectList] = useState([]);
   const getProject = async () => {
+    setIsLoading(true);
     const projectList = await axios.get(
       "https://portfolio-adnan-160125.herokuapp.com/api/project-list"
     );
     setProjectList(projectList.data);
+    setIsLoading(false);
   };
   useEffect(() => {
     getProject();
@@ -20,6 +23,13 @@ function Projects() {
   return (
     <div className="projects">
       <h1> My Personal Projects</h1>
+      {isLoading && (
+        <MoonLoader color="#21325e">
+          size={100}
+          loading={isLoading}
+        </MoonLoader>
+      )}
+
       <div className="projectList">
         {projectList.map((project, _id) => {
           return <ProjectItem id={project._id} project={project} />;
